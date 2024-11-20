04b747a4e58fde398952221e272c129d39cfd961
윤여원 <dudnjsckrgo@gmail.com>
2024-11-20 15:24:34 +0900

    fix: stream

--- src/main/kotlin/com/example/kotlin/chat/service/UserStatusMonitoringService.kt
+++ src/main/kotlin/com/example/kotlin/chat/service/UserStatusMonitoringService.kt
@@ -1,6 +1,7 @@
 package com.example.kotlin.chat.service
 import io.micrometer.core.instrument.MeterRegistry
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.runBlocking
 import org.springframework.stereotype.Service
 
 @Service
@@ -14,14 +15,15 @@ class UserStatusMonitoringService(private val meterRegistry: MeterRegistry) {
         meterRegistry.gauge("authenticated_users", this) { it.authenticatedUserCount }
         meterRegistry.gauge("anonymous_users", this) { it.anonymousUserCount }
     }
+
     private fun getCurrentUserCount(): UserCount {
         val authenticatedCount = getAuthenticatedUserCount()
         val anonymousCount = getAnonymousUserCount()
         return UserCount(authenticatedCount, anonymousCount)
     }
+
     // 실시간 사용자 수 상태를 위한 MutableSharedFlow 생성
     private val userCountFlow = MutableSharedFlow<UserCount>(replay = 1)
-
 
     /**
      * 최신 사용자 상태를 반환하는 함수
@@ -33,30 +35,39 @@ class UserStatusMonitoringService(private val meterRegistry: MeterRegistry) {
     /**
      * 실시간 사용자 상태 스트림을 반환하는 함수
      */
-    fun stream(): Flow<UserCount> = userCountFlow
-        .asSharedFlow()
-
+    fun stream(): Flow<UserCount> = userCountFlow.asSharedFlow()
 
     // 인증 사용자 증가
     fun incrementAuthenticatedUser() {
         authenticatedUserCount += 1
+        emitUserCount()
     }
 
     // 인증 사용자 감소
     fun decrementAuthenticatedUser() {
         authenticatedUserCount -= 1
+        emitUserCount()
     }
 
     // 익명 사용자 증가
     fun incrementAnonymousUser() {
         anonymousUserCount += 1
+        emitUserCount()
     }
 
     // 익명 사용자 감소
     fun decrementAnonymousUser() {
         anonymousUserCount -= 1
+        emitUserCount()
+    }
+
+    private fun emitUserCount() {
+        runBlocking {
+            userCountFlow.emit(getCurrentUserCount())
+        }
     }
 
     fun getAuthenticatedUserCount(): Int = authenticatedUserCount.toInt()
     fun getAnonymousUserCount(): Int = anonymousUserCount.toInt()
 }
+
